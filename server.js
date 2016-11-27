3509a22250d024f628679d0fd385eb4cb80e84fc
Vis Vasanth <viscodesdpt@gmail.com>
2016-11-26 18:36:36 -0600

    packages added,all burgers showon screen UNFORMATTED, handlebar files added

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 var PORT = 8000;
 var express = require('express');
 var app = express();
-
+var path = require('path');
+var methodOverride = require('method-override'); // for deletes in express
 var models = require('./models');
+
+
+// Our model controllers (rather than routes)
+var application_controller = require('./controllers/application_controller');
+
+// override POST to have DELETE and PUT
+app.use(methodOverride('_method'))
 
 models.sequelize.sync();
 //connects to table in db, then aut generates a table in the db
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+//set up handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 
 app.get('/', function(req, res) {
     models.Burger.findAll()
@@ -24,3 +43,5 @@ app.get('/', function(req, res) {
 app.listen(process.env.PORT || PORT, function() {
     console.log("24K Magic Happens on PORT: " + PORT);
 });
+
+module.exports = app;
